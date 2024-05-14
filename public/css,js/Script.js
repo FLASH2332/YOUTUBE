34fff7d6936e6fadb0530b96fc46d08403e7a206
FLASH <148314235+FLASH2332@users.noreply.github.com>
2024-05-14 15:12:48 +0530

    Update Script.js

--- public/css,js/Script.js
+++ public/css,js/Script.js
@@ -1,51 +1,5 @@
-const toggleBtn = document.getElementById('toggleBtn');
-const sidebar = document.querySelector('.side-bar');
-const linkTexts = document.querySelectorAll('.link-text');
-const linkers=document.getElementsByClassName("links")
 const videoContainer = document.getElementById('video-section');
 const videocard=document.getElementsByClassName('video-container');
-var temp=window.innerWidth>800 ? false : true;
-
-
-sidebar.classList.toggle('show');
-var clicked=false;
-toggleBtn.addEventListener('click', () => {
-    clicked=true;
-    linkTexts.forEach(linkText => {
-        linkText.classList.toggle('show-text');
-    });
-    document.getElementById("cover").classList.toggle("show");
-    if(window.innerWidth<800){
-    sidebar.classList.toggle("show");
-}
-console.log(temp);
-
-console.log(clicked);
-}
-);window.addEventListener('resize', () => {
-    if (window.innerWidth < 800 && !temp) {
-        sidebar.classList.remove('show');
-        linkTexts.forEach(linkText => {
-            linkText.classList.remove('show-text');
-        });
-        document.getElementById("cover").classList.remove("show");
-        temp = true;
-    } else if (window.innerWidth >= 800 && temp) {
-        sidebar.classList.add('show');
-        temp = false;
-    }
-});
-
-
-document.getElementById("cover").addEventListener('click', () => {
-    if(clicked==true){
-        document.getElementById("cover").classList.toggle("show");
-        linkTexts.forEach(linkText => {
-            linkText.classList.toggle('show-text');
-        });
-        clicked=false;
-    }
-});
 
 
 let lastLoadedIndex = 0; 
@@ -82,36 +36,84 @@ const loadMoreVideos = () => {
 
 loadMoreVideos();
 
+function returnDateForDesc(date) {
+    let minute = Math.floor(date / 60);
+    let hour = Math.floor(minute / 60);
+    let day = Math.floor(hour / 24);
+    let month = Math.floor(day / 30);
+    let year = Math.floor(month / 12);
+
+    if (year < 1) {
+        if (month < 1) {
+            if (day < 1) {
+                if (hour < 1)
+                    return minute + " minutes ago";
+                return hour + " hours ago";
+            }
+            return day + " days ago";
+        }
+        return month + " months ago";
+    }
+    return year + " years ago";
+}
 
 
 const makeVideoCard = (data) => {
     return `
     <div class="video-container">
-    <img class="videos"  src='${data.id}'>
+        <div class="videoContainer">
+            <img class="videos"  src='${data.id}'>
+            <video class="video">
+                <source src="" type="video/mp4">
+            </video>
+        </div>
     <div class="video-description"> 
     <div class="channel-dp-container-2"> 
         <img src="${data.channelThumbnail}" class="channel-dp-2" alt="">
     </div> 
         <div class="video-details">
-                <a class="video-title">${data.title}</a>
-                <p class="channel-name">${data.channelName}</p>
-                <p class="views">${data.views}</p>
-                <p class="time">${data.time}</p>
+                <a class="video-title" >${data.title}</a>
+                <p class="channel-name" >${data.channelName}</p>
+                <p class="views">${data.views}</p> 
+                <p class="time">${returnDateForDesc(parseInt(data.time))}</p>
         </div>
     </div>
     </div>`;
 };
 
-
-
-// videoContainer.addEventListener('click', () => {
-//     if(clicked===true){
-//     linkTexts.forEach(linkText => {
-//         linkText.classList.toggle('show-text');
-        
+// document.addEventListener("DOMContentLoaded", function() {
+//     console.log("DOM Loaded");
+//     const arr = document.querySelectorAll('.video-container');
+//     console.log("Number of video containers found:", arr.length);
+//     Array.from(arr).forEach(element => {
+//         console.log("Processing video container");
+//         element.addEventListener("mouseover", async function(event) {
+//             console.log("Mouseover event");
+//             const video = element.querySelector(".video");
+//             if (!video) {
+//                 console.error("Video element not found");
+//                 return;
+//             }
+//             const source = video.querySelector("source");
+//             if (!source || !source.src) {
+//                 console.error("Video source not found or empty");
+//                 return;
+//             }
+//             video.play();
+//             console.log("Video played");
+//         });
+//         element.addEventListener("mouseout", function(event) {
+//             console.log("Mouseout event");
+//             const video = element.querySelector(".video");
+//             if (video) {
+//                 video.pause();
+//                 video.currentTime = 0;
+//                 console.log("Video paused and reset");
+//             }
+//         });
 //     });
-//     clicked=false;
-// }
 // });
+
+
 
 
