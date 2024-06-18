bd4cb1c7bb0b0bbfc0dbcfd8bd0296b95950ced3
Jayadev D <148314235+FLASH2332@users.noreply.github.com>
2024-06-18 10:46:44 +0530

    Add files via upload

---
+++ public/data/css,js/subscription/Subscription.js
@@ -0,0 +1,57 @@
+const channelContainer = document.getElementById('channel-section');
+
+/*
+let lastLoadedIndex = 0; 
+const channelsPerPage = 6; 
+
+window.addEventListener('scroll', () => {
+    const { scrollTop, scrollHeight, clientHeight } = document.documentElement;
+
+    if (scrollTop + clientHeight >= scrollHeight - 200) {
+        loadMorechannels();
+    }
+});
+
+const subscriptioncard =(data) =>{
+    return `
+    <div class="subscription-container">
+    <img class="profile-pic" src='${data.channelThumbnail}'>
+    <div class="details">
+        <p class="channel-name">${data.title}</p>
+        <div class="count">
+        <div class="views">${data.views}</div>&nbsp;•&nbsp;<div class="subscriber-count">${data.subscribers}</div>
+        </div>
+        <div class="description"><p>${data.description}</p></div>
+    </div>
+</div>
+    `;
+}
+
+
+const loadMorechannels = () => {
+    fetch('public/data/subscriptions.json')
+        .then(response => response.json())
+        .then(channelData => {
+            const totalchannels = channelData.length;
+            if (lastLoadedIndex < totalchannels) {
+                const remainingchannels = totalchannels - lastLoadedIndex;
+                const channelsToLoad = remainingchannels > channelsPerPage ? channelsPerPage : remainingchannels;
+                const channelSlice = channelData.slice(lastLoadedIndex, lastLoadedIndex + channelsToLoad);
+                channelSlice.forEach(channel => {
+                    const subscriptioncardhtml = subscriptioncard(channel);
+                    channelContainer.innerHTML += subscriptioncardhtml;
+                });
+                lastLoadedIndex += channelsToLoad;
+            }
+        })
+        .catch(error => {
+            console.error('Error fetching JSON data:', error);
+        });
+};
+
+loadMorechannels();
+
+*/
+
+
+
